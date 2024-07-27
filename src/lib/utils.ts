a07f615c297d317dd75ce90b47462383044ce3aa
Keyun Simejiya <keyunsimejiya@Keyuns-MacBook-Air.local>
2024-07-27 11:04:33 +0530

    minor update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -39,9 +39,9 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: '@joshtriedcoding',
+      creator: '@keyunsimejiya',
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/")
+    metadataBase: new URL("https://casecobra-cover-store.vercel.app/")
   }
 }
